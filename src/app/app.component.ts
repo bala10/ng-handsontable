686d1493654492c21ab7e96dfed309e9bcb275fb
Balasubramanian <ebalasubramanian24@gmail.com>
2018-07-24 15:05:47 +0530

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,7 +4,7 @@ import * as Handsontable from 'handsontable-pro';
 function titleRenderer(instance, td, row, col, prop, value, cellProperties) {
   Handsontable.renderers.TextRenderer.apply(this, arguments);
   Handsontable.dom.fastInnerHTML(td, '<h3>' + value +'('+ instance.alignText +')'+'</h3>');
-  td.style.textAlign = instance.alignText;
+  td.style.textAlign = 'center';
 }
 @Component({
   selector: 'app-root',
@@ -16,7 +16,7 @@ export class AppComponent {
   private cells?: (row?: number, col?: number, prop?: object) => {};
   public alignText: string;
   constructor() {
-    this.alignText='center';
+    this.alignText='m3';
     this.data = [['Header'],
                   ['Body']];
     this.cells = (row, col, prop): any => {
